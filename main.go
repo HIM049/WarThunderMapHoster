5e6f5aa5413ccdca3ff26bae90a11355b32cdec7
HIM049 <HIM_049@163.com>
2025-05-08 19:40:31 +0800

    fix: server crash

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"thunder_hoster/config"
+	"thunder_hoster/public"
 )
 
 func main() {
 	config.InitConfig()
+	public.InitFailedCounter()
 
 	router := RouterSetup()
 	router.Run(fmt.Sprintf(":%d", config.Cfg.Port))
